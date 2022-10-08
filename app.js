538321dec7fd981e3648c57c5139ff1b087f1203
bollowatsch <stephan.bollauf@gmail.com>
2022-10-08 13:24:20 +0200

    add Calculation object and history calculation data

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const display = document.querySelector(".display");
 const btnCl = document.querySelector("#btn-cl");
-const buttons = document.querySelectorAll(".btn-number");
+const btnsNumber = document.querySelectorAll(".btn-number");
+const btnsOperator = document.querySelectorAll(".btn-operator");
 
-let firstOperand, secondOperand;
+function Calculation(firstOperand, secondOperand, operation, result) {
+  this.firstOperand = firstOperand,
+    this.secondOperand = secondOperand,
+    this.operation = operation,
+    this.result = result
+};
+
+let calculationHistory = [];
+calculationHistory.push(new Calculation());
 
 
-for (btn of buttons) btn.addEventListener("click", (e) => {
+for (btn of btnsNumber) btn.addEventListener("click", (e) => {
   display.innerText = display.innerText.concat(e.target.dataset.id);
 });
 
-btnCl.addEventListener("click", clearDisplay);
+for (btn of btnsOperator) btn.addEventListener("click", (e) => {
+  display.innerText = display.innerText.concat(e.target.dataset.id);
+});
+
+//clear Display and create new Calculation. Either save the last one (if result could be determined) or overwrite, if not
+btnCl.addEventListener("click", () => {
+  clearDisplay();
+  calculationHistory[calculationHistory.length - 1].result === undefined ? calculationHistory[calculationHistory.length - 1] = new Calculation() : calculationHistory.push(new Calculation);
+});
 /*btnAdd.addEventListener("click",)
 <btnSubtract.addEventListener("click",)
 btnMultiply.addEventListener("click",)
@@ -22,6 +39,7 @@ function multiply(a, b) { return a * b; };
 function divide(a, b) { return a / b };
 
 function operate(operator, a, b) {
+  if (!(numberIsValid(a) && numberIsValid(b))) return NaN;
   switch (operator) {
     case add: return add(a, b);
     case subtract: return subtract(a, b);
