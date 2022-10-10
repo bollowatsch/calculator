8c29a2287b96495ad3855dc72db7e2c201aa7726
bollowatsch <stephan.bollauf@gmail.com>
2022-10-10 09:07:29 +0200

    add division by 0 support

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ### Implemented
 - historic data is stored (session only)
 - works with chained calculations
+- dividing by zero shows error message
 ### To be implemented
 - prevent overflow with rounding
 - keyboard support
-- error throwing/catching for dividing by zero
 - resize display text if it overflows
--- app.js
+++ app.js
@@ -62,7 +62,7 @@ btnCl.addEventListener("click", () => {
 function add(a, b) { return a + b };
 function subtract(a, b) { return a - b };
 function multiply(a, b) { return a * b; };
-function divide(a, b) { return a / b };
+function divide(a, b) { return b === 0 ? "error" : a / b }
 
 let calculationHistory = [];
 calculationHistory.push(new Calculation());
